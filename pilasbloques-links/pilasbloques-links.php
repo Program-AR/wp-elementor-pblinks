d4f08fd360a674f481e24dd7daccaf51c08f2dd1
root <root@fs-webs>
2024-01-11 10:10:25 -0500

    bump 2.3.3

--- pilasbloques-links/pilasbloques-links.php
+++ pilasbloques-links/pilasbloques-links.php
@@ -88,5 +88,5 @@ Class PilasBloquesVersion extends \Elementor\Core\DynamicTags\Tag {
 }
 // ---------------------------------------------------
 
-$pilasBloquesVersion = '2.1.9';
+$pilasBloquesVersion = '2.3.3';
 
